a280248ab26b9db9b44b1d3faba44406ab3e84f8
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 13:41:35 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -4,10 +4,20 @@ from efficientnet_pytorch.model import EfficientNet
 from sotabench.image_classification import imagenet
 
 def benchmark():
+    
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    input_transform = transforms.Compose([
+        transforms.Resize(256, 'PIL.Image.BICUBIC'),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    
     imagenet.benchmark(
         model=EfficientNet.from_pretrained(model_name='efficientnet-b0'),
         paper_model_name='EfficientNet B0',
         paper_arxiv_id='1905.11946',
         paper_pwc_id='efficientnet-rethinking-model-scaling-for',
+        input_transform=input_transform,
         batch_size=32
     )

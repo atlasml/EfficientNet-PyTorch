1f5c754c19f47552b5e24366d4c73eeaf24a7662
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 13:05:13 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -10,24 +10,3 @@ def benchmark():
         paper_arxiv_id='1905.11946',
         paper_pwc_id='efficientnet-rethinking-model-scaling-for'
     )
-    
-    imagenet.benchmark(
-        model=EfficientNet.from_pretrained(model_name='efficientnet-b1'),
-        paper_model_name='EfficientNet B1',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )
-    
-    imagenet.benchmark(
-        model=EfficientNet.from_pretrained(model_name='efficientnet-b2'),
-        paper_model_name='EfficientNet B2',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )
-    
-    imagenet.benchmark(
-        model=EfficientNet.from_pretrained(model_name='efficientnet-b3'),
-        paper_model_name='EfficientNet B3',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )

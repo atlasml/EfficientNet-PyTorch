203eef7b635c6537553d9f9a35c68bfd98816cf2
Ross Taylor <rj-taylor@live.co.uk>
2019-07-12 12:44:19 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -15,7 +15,40 @@ input_transform = transforms.Compose([
 # Run Evaluation
 ImageNet.benchmark(
     model=EfficientNet.from_pretrained(model_name='efficientnet-b0'),
-    paper_model_name='EfficientNet',
+    paper_model_name='EfficientNet-B0',
+    paper_arxiv_id='1905.11946',
+    paper_pwc_id='efficientnet-rethinking-model-scaling-for',
+    input_transform=input_transform,
+    batch_size=256,
+    num_gpu=1
+)
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=EfficientNet.from_pretrained(model_name='efficientnet-b1'),
+    paper_model_name='EfficientNet-B1',
+    paper_arxiv_id='1905.11946',
+    paper_pwc_id='efficientnet-rethinking-model-scaling-for',
+    input_transform=input_transform,
+    batch_size=256,
+    num_gpu=1
+)
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=EfficientNet.from_pretrained(model_name='efficientnet-b2'),
+    paper_model_name='EfficientNet-B2',
+    paper_arxiv_id='1905.11946',
+    paper_pwc_id='efficientnet-rethinking-model-scaling-for',
+    input_transform=input_transform,
+    batch_size=256,
+    num_gpu=1
+)
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=EfficientNet.from_pretrained(model_name='efficientnet-b3'),
+    paper_model_name='EfficientNet-B3',
     paper_arxiv_id='1905.11946',
     paper_pwc_id='efficientnet-rethinking-model-scaling-for',
     input_transform=input_transform,

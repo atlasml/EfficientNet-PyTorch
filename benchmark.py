08d8f68492bf7e665c9441c38999093ffb6e24f2
Ross Taylor <rj-taylor@live.co.uk>
2019-07-15 14:38:23 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -5,7 +5,7 @@ import PIL
 
 # Define Transforms    
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-input_transform = transforms.Compose([
+b0_input_transform = transforms.Compose([
     transforms.Resize(256, PIL.Image.BICUBIC),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
@@ -18,10 +18,18 @@ ImageNet.benchmark(
     paper_model_name='EfficientNet-B0',
     paper_arxiv_id='1905.11946',
     paper_pwc_id='efficientnet-rethinking-model-scaling-for',
-    input_transform=input_transform,
+    input_transform=b0_input_transform,
     batch_size=256,
     num_gpu=1
 )
+
+# Define Transforms    
+b1_input_transform = transforms.Compose([
+    transforms.Resize(273, PIL.Image.BICUBIC),
+    transforms.CenterCrop(240),
+    transforms.ToTensor(),
+    normalize,
+])
 
 # Run Evaluation
 ImageNet.benchmark(
@@ -29,10 +37,18 @@ ImageNet.benchmark(
     paper_model_name='EfficientNet-B1',
     paper_arxiv_id='1905.11946',
     paper_pwc_id='efficientnet-rethinking-model-scaling-for',
-    input_transform=input_transform,
+    input_transform=b1_input_transform,
     batch_size=256,
     num_gpu=1
 )
+
+# Define Transforms    
+b2_input_transform = transforms.Compose([
+    transforms.Resize(292, PIL.Image.BICUBIC),
+    transforms.CenterCrop(260),
+    transforms.ToTensor(),
+    normalize,
+])
 
 # Run Evaluation
 ImageNet.benchmark(
@@ -40,7 +56,7 @@ ImageNet.benchmark(
     paper_model_name='EfficientNet-B2',
     paper_arxiv_id='1905.11946',
     paper_pwc_id='efficientnet-rethinking-model-scaling-for',
-    input_transform=input_transform,
+    input_transform=b2_input_transform,
     batch_size=256,
     num_gpu=1
 )
@@ -51,7 +67,7 @@ ImageNet.benchmark(
     paper_model_name='EfficientNet-B3',
     paper_arxiv_id='1905.11946',
     paper_pwc_id='efficientnet-rethinking-model-scaling-for',
-    input_transform=input_transform,
+    input_transform=b1_input_transform,
     batch_size=256,
     num_gpu=1
 )
